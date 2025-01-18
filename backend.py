23f81f9f806d3f94058f11066b9c1780b0562122
habdulhaq87 <134699395+habdulhaq87@users.noreply.github.com>
2025-01-18 06:27:49 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -105,7 +105,7 @@ def update_task_budget_and_timeline(
         """
         UPDATE subtasks
         SET
-            Budget = ?,
+            budget = ?,
             [Start Time] = ?,
             Deadline = ?
         WHERE id = ?
